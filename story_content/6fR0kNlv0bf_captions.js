f6391ffdf242f241321de6f7ee90d0c02d982178
MindMates <daniela.akh@outlook.com>
2024-10-25 14:09:06 +0200

    Delete story_content directory

--- story_content/6fR0kNlv0bf_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.92.33225.0%0D%0A%0D%0A00:00:00.075%20--%3E%2000:00:02.526%0D%0AThe%20second%20step,%20you%20have%20to%20Make%20a%20fist.%0D%0A%0D%0A"}')
